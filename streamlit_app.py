62ffbe365bf7b2d4e62eeca20c8cfd8cfea48580
pathakaniket24 <141309938+pathakaniket24@users.noreply.github.com>
2023-08-11 23:39:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruis_to_show)
 
 # New Section 
 streamlit.header("Fruityvice Fruit Advice!")
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
